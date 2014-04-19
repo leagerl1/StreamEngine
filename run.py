bc86bc8c198062992df603d74a427528799c1ce0
leagerl1 <eagerleavittlu@gmail.com>
2014-04-19 16:29:09 -0400

    Added Twilio test

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask, render_template, request, redirect
+import twilio.twiml
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
