097569b433819a08a520c0833fae3d879a2860ac
leagerl1 <eagerleavittlu@gmail.com>
2014-04-19 21:31:57 -0400

    Returns single text

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ def responder():
     account_sid = "ACfe656ed49f19a12b8440cb191158f0c9"
     auth_token = "8d29d5cc81e4062e1521237983c39b21"
     client = TwilioRestClient(account_sid, auth_token)
-    sms = client.sms.messages.get("SM800f449d0399ed014aae2bcc0cc2f2ec")
-    print sms.body
+    sms = client.messages.get("SMbb7efe0e6b3c545245ee6d6cb5944364")
+    return message.body
 
 @app.route('/')
 def index():
