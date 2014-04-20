6fffd21994121c89f012c040c3db8c8e1e8d552f
leagerl1 <eagerleavittlu@gmail.com>
2014-04-19 20:50:03 -0400

    Changed to read text

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 import os
 from flask import Flask, render_template, request, redirect
+from twilio.rest import TwilioRestClient
 import twilio.twiml
 
 app = Flask(__name__)
 
 @app.route('/_responder', methods=['GET','POST'])
 def responder():
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    return str(resp)
+    account_sid = "ACfe656ed49f19a12b8440cb191158f0c9"
+    auth_token = "8d29d5cc81e4062e1521237983c39b21"
+    client = TwilioRestClient(account_sid, auth_token)
+    sms = client.sms.messages.get("SM800f449d0399ed014aae2bcc0cc2f2ec")
+    return sms.body
 
 @app.route('/')
 def index():
