0370c2f6686c308756748f0094b289832df7c50f
leagerl1 <eagerleavittlu@gmail.com>
2014-04-22 18:42:03 -0400

    Fixed body request

--- run.py
+++ run.py
@@ -9,9 +9,8 @@ app = Flask(__name__)
 def responder():
     account_sid = "ACfe656ed49f19a12b8440cb191158f0c9"
     auth_token = "8d29d5cc81e4062e1521237983c39b21"
-    client = TwilioRestClient(account_sid, auth_token)
-    sms = client.messages.get("SMbb7efe0e6b3c545245ee6d6cb5944364")
-    return message.body
+    message = request.values.get('Body')
+    return message
 
 @app.route('/')
 def index():
