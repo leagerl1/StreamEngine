2a7f79209ea4c80ebf5f048c8c863ccf275bab06
leagerl1 <eagerleavittlu@gmail.com>
2014-04-22 19:16:06 -0400

    Switched out of index

--- run.py
+++ run.py
@@ -7,19 +7,14 @@ app = Flask(__name__)
 
 @app.route('/_responder', methods=['GET','POST'])
 def responder():
-    resp = twilio.twiml.Response()
-    #message = request.values.get('Body')
-    message = "Whats up"
-    resp.message("Hello")
-    return str(message)
-
-@app.route('/')
-def index():
     account_sid = "AC5ef8732a3c49700934481addd5ce1659"
     auth_token = "8d29d5cc81e4062e1521237983c39b21"
     client = TwilioRestClient(account_sid,auth_token)
     sms = client.sms.messages.get("SM800f449d0399ed014aae2bcc0cc2f2ec")
-    print sms.body
+    print sms.body   
+
+@app.route('/')
+def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
