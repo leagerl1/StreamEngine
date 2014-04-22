ab6fbf5fc099928adddc7ce704df900be9a1908c
leagerl1 <eagerleavittlu@gmail.com>
2014-04-22 19:10:36 -0400

    Testing within index run

--- run.py
+++ run.py
@@ -15,6 +15,11 @@ def responder():
 
 @app.route('/')
 def index():
+    account_sid = "AC5ef8732a3c49700934481addd5ce1659"
+    auth_token = "8d29d5cc81e4062e1521237983c39b21"
+    client = TwilioRestClient(account_sid,auth_token)
+    sms = client.sms.messages.get("SM800f449d0399ed014aae2bcc0cc2f2ec")
+    print sms.body
     return render_template('index.html')
 
 if __name__ == "__main__":
