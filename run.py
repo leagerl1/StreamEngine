25667f31845c600cfe4ddfc490910fc5842d6cee
leagerl1 <eagerleavittlu@gmail.com>
2014-04-22 20:13:19 -0400

    HTTP Requests version

--- run.py
+++ run.py
@@ -2,16 +2,16 @@ import os
 from flask import Flask, render_template, request, redirect
 from twilio.rest import TwilioRestClient
 import twilio.twiml
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/_responder', methods=['GET','POST'])
 def responder():
-    account_sid = "AC5ef8732a3c49700934481addd5ce1659"
-    auth_token = "8d29d5cc81e4062e1521237983c39b21"
-    client = TwilioRestClient(account_sid,auth_token)
-    sms = client.sms.messages.get("SM800f449d0399ed014aae2bcc0cc2f2ec")
-    print sms.body   
+    ec_r = requests.get("https://ACfe656ed49f19a12b8440cb191158f0c9:8d29d5cc81e4062e1521237983c39b21@api.twilio.com/2010-04-01/Accounts/ACfe656ed49f19a12b8440cb191158f0c9/SMS/Messages.json")
+    data = json.loads(ec_r.text)
+    return data['sms_messages'][0]['body']
 
 @app.route('/')
 def index():
