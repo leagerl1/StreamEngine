d2b3c807d830c676b2ca1c2b5a12ed253330182f
leagerl1 <eagerleavittlu@gmail.com>
2014-04-30 12:50:22 -0400

    Added twilio response

--- run.py
+++ run.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 
 @app.route('/_responder', methods=['GET','POST'])
 def responder():
+    resp = twilio.twiml.Response()
+    resp.message("Thank you! Your song has been added to StreamEngine")
     ec_r = requests.get("https://ACfe656ed49f19a12b8440cb191158f0c9:8d29d5cc81e4062e1521237983c39b21@api.twilio.com/2010-04-01/Accounts/ACfe656ed49f19a12b8440cb191158f0c9/SMS/Messages.json")
     data = json.loads(ec_r.text)
     song = data['sms_messages'][0]['body']
