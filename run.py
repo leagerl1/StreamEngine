714fcd609755866fb1204d2f786b844650b4dfa1
leagerl1 <eagerleavittlu@gmail.com>
2014-05-01 22:47:04 -0400

    Adding tests to app

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ from rdio import Rdio
 
 app = Flask(__name__)
 
-def render_index():
-    return render_template('index.html')
+def render_index(track):
+    return render_template('app.html',song=track)
 
 @app.route('/_text',methods=['GET','POST'])
 def hello_monkey():
@@ -19,7 +19,6 @@ def hello_monkey():
     txt = str(sum)
     resp = twilio.twiml.Response()
     resp.message(txt)
-    render_index()
     return str(resp) 
 
 @app.route('/_responder', methods=['GET','POST'])
@@ -27,7 +26,10 @@ def responder():
     ec_r = requests.get("https://ACfe656ed49f19a12b8440cb191158f0c9:8d29d5cc81e4062e1521237983c39b21@api.twilio.com/2010-04-01/Accounts/ACfe656ed49f19a12b8440cb191158f0c9/SMS/Messages.json")
     data = json.loads(ec_r.text)
     song = data['sms_messages'][0]['body']
-    return render_template('app.html', song = song)
+    render_index(song)
+    resp = twilio.twiml.Response()
+    resp.message("Your song will be added")
+    return str(resp)
 
 @app.route('/')
 def index():
