a14df56815a7136dc824d2ca386a07b0852739f8
leagerl1 <eagerleavittlu@gmail.com>
2014-05-01 22:24:28 -0400

    More tests

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ def hello_monkey():
     txt = str(sum)
     resp = twilio.twiml.Response()
     resp.message(txt)
-#    return str(resp)
+    return str(resp)
 
 @app.route('/_responder', methods=['GET','POST'])
 def responder():
