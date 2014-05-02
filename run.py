e8bee1caaf281848612be7b71bfe8ffe06a6e17b
leagerl1 <eagerleavittlu@gmail.com>
2014-05-01 22:11:09 -0400

    Test text response

--- run.py
+++ run.py
@@ -8,6 +8,12 @@ from rdio import Rdio
 
 app = Flask(__name__)
 
+@app.route('/_text',methods=['GET','POST'])
+def hello_monkey():
+    resp = twilio.twiml.Response()
+    resp.message("Hello, mobile monkey")
+    return str(resp)
+
 @app.route('/_responder', methods=['GET','POST'])
 def responder():
     ec_r = requests.get("https://ACfe656ed49f19a12b8440cb191158f0c9:8d29d5cc81e4062e1521237983c39b21@api.twilio.com/2010-04-01/Accounts/ACfe656ed49f19a12b8440cb191158f0c9/SMS/Messages.json")
